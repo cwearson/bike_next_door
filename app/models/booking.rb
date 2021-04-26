356c96c79ef9f7c952613a1d870c3c65d655ba96
kathf <fleischerkath@gmail.com>
2021-04-26 12:11:39 +1000

    Validate booking date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,12 @@
 class Booking < ApplicationRecord
+  belongs_to :bike
+
+  validates :date, :user_full_name, presence: true
+  validate :today_or_in_future
+
+  def today_or_in_future
+    if date < Date.current
+      errors.add(:date, "can't be in the past")
+    end
+  end
 end
